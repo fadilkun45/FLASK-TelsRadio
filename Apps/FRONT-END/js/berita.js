771bf524dd1652352e3e101a50b0d6b0bbb69357
fadilkun45 <indocrafter76@gmail.com>
2021-01-08 18:57:38 +0700

    update tombol scroll up

--- Apps/FRONT-END/js/berita.js
+++ Apps/FRONT-END/js/berita.js
@@ -1,7 +1,7 @@
 const toggleNaik = document.querySelector(".toggle-naik");
-// const container = document.querySelector('.container');
 
 
+if (document.documentElement.clientWidth < 800) {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -18,3 +18,4 @@ toggleNaik.addEventListener ('click' ,function () {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 })
+}

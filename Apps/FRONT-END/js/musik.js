8f08774eaf3083618f050356568b7d451458af6a
fadilkun45 <indocrafter76@gmail.com>
2021-04-16 04:22:44 +0700

    update

--- Apps/FRONT-END/js/musik.js
+++ Apps/FRONT-END/js/musik.js
@@ -5,6 +5,7 @@ const GenreContent = document.querySelectorAll(".genre-content");
 const renderResult = document.querySelector("#genre-render");
 const GenreJudulRender = document.querySelector(".genre-judul");
 const RenderInfo = document.querySelector("#RenderInfo");
+const SearchBar = document.querySelector('#bar');
 // musik player
 const load = document.querySelector(".load");
 const MusikTrack = document.querySelector("#musik-track");
@@ -72,9 +73,11 @@ GenreContent.forEach((content) => {
         listrender(data);
         GenreJudulRender.innerHTML = `<h4>${judul}</h4>`;
         musik.length = 0;
-        console.log(musik.length);
+        // console.log(musik.length);
     });
 });
+
+
 
 var judul = [];
 var channel = [];
@@ -91,10 +94,204 @@ loadAll.addEventListener("click", () => {
     renderResult.innerHTML = "";
 });
 
+
+
+
+
 function listrender(data) {
+    // ini searchbar
+    SearchBar.addEventListener('keyup',(event) => {
+        if(event.keyCode == 13) {
+            renderResult.innerHTML = `                <div class="animasi">
+            <div class="kotak"></div>
+            <div class="title"></div>
+        </div>     
+        <div class="animasi">
+            <div class="kotak"></div>
+            <div class="title"></div>
+        </div>   
+        <div class="animasi">
+            <div class="kotak"></div>
+            <div class="title"></div>
+        </div>   
+        <div class="animasi">
+            <div class="kotak"></div>
+            <div class="title"></div>
+        </div>      `
+        const valueRaw = SearchBar.value ;
+        let valueResult = valueRaw.trim().toLowerCase().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
+            console.log(valueResult)
+            fetch(`http://api-tomcatsquad.herokuapp.com/api/v1/music/?g=${data}&t=${valueResult}`)
+            .then((res) => res.json())
+            .then((res) => {
+              console.log(valueResult)
+                renderResult.innerHTML = "";
+                judul = [];
+                channel = [];
+                thumbnail = [];
+                for (i = 0; i < res["results"].length; i++) {
+                    musik.push(res["results"][i]["url"]);
+                    judul.push(res["results"][i]["title"]);
+                    channel.push(res["results"][i]["channel_name"]);
+                    thumbnail.push(res["results"][i]["thumbnail"]);
+                    renderResult.innerHTML += `<div class="musik-box" urutan="${i}" judul="${judul[i]}" thumbnail="${thumbnail[i]}" channel="${channel[i]}">
+                    <div class="sec-1">
+                        <img src="${thumbnail[i]}">
+                        <div class="judul-dan-channel">
+                            <h4>${judul[i]}</h4>
+                            <p>${channel[i]}</p>
+                        </div>
+                    </div>
+                </div>`;
+                }
+
+                
+    
+    
+    
+                function nextmusic() {
+                    currentsong++
+                    if (currentsong > musik.length - 1) {
+                        currentsong = 0;
+                    }
+    
+                    setelmusic();
+                    RenderInfo.innerHTML = `<div class="musik-info">
+                    <img src="${thumbnail[currentsong]}" id="thumb">
+                    <div class="sec-1">
+                    <div class="judul-artis">
+                    <marquee behavior = "scroll"
+                    scrollamount="6">${judul[currentsong]}</marquee>
+                    <p>${channel[currentsong]}</p>
+                    </div>
+                    </div>`;
+                    Playbtn.innerHTML = `<img src="img/pause.png">`;
+                }
+    
+                nextbtn.addEventListener("click", () => {
+                    nextmusic();
+                });
+                prevbtn.addEventListener("click", () => {
+                    prevmusic();
+                });
+    
+                function setelmusic() {
+    
+                    audio.src = musik[currentsong];
+                    audio.play();
+                    playbtn.innerHTML = `<img src="img/pause.png"/>`;
+                }
+    
+                calling();
+    
+                MusikTrack.addEventListener("change", () => {
+                    audio.currentTime = MusikTrack.value;
+                });
+    
+                function timeFormat(time) {
+                    return (
+                        Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+                    );
+                }
+    
+                audio.addEventListener("timeupdate", () => {
+                    MusikTrack.value = audio.currentTime;
+                    MusikTime.innerText = `${timeFormat(audio.currentTime)}`;
+                    MusikTotal.innerText = `${timeFormat(audio.duration)}`;
+                    let position = audio.currentTime / audio.duration;
+                    load.style.width = position * 100 + "%";
+                    if (audio.ended) {
+                        nextmusic()
+                        RenderInfo.innerHTML = `<div class="musik-info">
+                  <img src="${thumbnail[currentsong]}" id="thumb">
+                  <div class="sec-1">
+                      <div class="judul-artis">
+                      <marquee behavior = "scroll"
+                      scrollamount="6">${judul[currentsong]}</marquee>
+                          <p>${channel[currentsong]}</p>
+                      </div>
+                  </div>`;
+                        Playbtn.innerHTML = `<img src="img/pause.png">`;
+                    }
+                });
+    
+                audio.addEventListener("loadedmetadata", () => {
+                    MusikTrack.value = audio.currentTime;
+                    MusikTrack.setAttribute("max", audio.duration);
+                });
+    
+                Volume.addEventListener("mousemove", () => {
+                    calling();
+                });
+    
+                function calling() {
+                    audio.volume = Volume.value / 100;
+                    // Musikvalue.innerHTML = `<p>${Volume.value}</p>`;
+                    var x = Volume.value;
+                    var color =
+                        `linear-gradient(90deg, rgb(5,5,5)` +
+                        x +
+                        "%, rgb(219,219,219)" +
+                        x +
+                        "%)";
+                    Volume.style.background = color;
+                }
+    
+                function prevmusic() {
+                    currentsong-- ;
+                    if (currentsong > musik.length - 1 ) {
+                        currentsong = 1 ;
+                        // console.log(currentsong)
+                    }else if(currentsong == -1 ){
+                      currentsong =  musik.length - 1 ;
+                    }
+                    setelmusic();
+                    RenderInfo.innerHTML = `<div class="musik-info">
+                    <img src="${thumbnail[currentsong]}" id="thumb">
+                    <div class="sec-1">
+                        <div class="judul-artis">
+                        <marquee behavior = "scroll"
+                        scrollamount="6">${judul[currentsong]}</marquee>
+                            <p>${channel[currentsong]}</p>
+                        </div>
+                    </div>`;
+                    Playbtn.innerHTML = `<img src="img/pause.png">`;
+                   
+                }
+    
+                const MusikBox = document.querySelectorAll(".musik-box");
+                if( MusikBox.length == 0 ){
+                    renderResult.innerHTML = `<center class="notice">Maaf genre yang anda minta belum tersedia</center>`;
+                }
+                MusikBox.forEach((TesMusik) => {
+
+
+                    TesMusik.addEventListener("click", () => {
+                        currentsong = TesMusik.getAttribute("urutan");
+                        setelmusic();
+                        RenderInfo.innerHTML = `<div class="musik-info">
+             <img src="${TesMusik.getAttribute("thumbnail")}" id="thumb">
+             <div class="sec-1">
+                 <div class="judul-artis">
+                 <marquee behavior = "scroll"
+          scrollamount="6">${TesMusik.getAttribute("judul")}</marquee>
+                     <p>${TesMusik.getAttribute("channel")}</p>
+                 </div>
+             </div>`;
+                    });
+                });
+    
+                // console.log(musik.length - 1)
+    
+            }).catch((res) => {
+            });        };
+    })
+
+    // ini fetch default saat kotak genre dipilih
     fetch("https://api-tomcatsquad.herokuapp.com/api/v1/music/?genre=" + data)
         .then((res) => res.json())
         .then((res) => {
+          
             renderResult.innerHTML = "";
             judul = [];
             channel = [];
@@ -114,10 +311,14 @@ function listrender(data) {
                 </div>
             </div>`;
             }
-            console.log(musik.length)
+            // console.log(musik.length)
             if (musik.length == 0) {
                 renderResult.innerHTML = `<center class="notice">Maaf genre yang anda minta belum tersedia</center>`;
             }
+
+            
+
+
 
             function nextmusic() {
                 currentsong++
@@ -211,7 +412,7 @@ function listrender(data) {
                 currentsong-- ;
                 if (currentsong > musik.length - 1 ) {
                     currentsong = 1 ;
-                    console.log(currentsong)
+                    // console.log(currentsong)
                 }else if(currentsong == -1 ){
                   currentsong =  musik.length - 1 ;
                 }
@@ -246,7 +447,7 @@ function listrender(data) {
                 });
             });
 
-            console.log(musik.length - 1)
+            // console.log(musik.length - 1)
 
         }).catch((res) => {
             console.log(res);

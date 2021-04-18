3e6fd1ff4bb1eac46a1f8fcecf370a72b3735138
fadilkun45 <indocrafter76@gmail.com>
2021-04-18 17:17:18 +0700

    update

--- Apps/FRONT-END/js/radio.js
+++ Apps/FRONT-END/js/radio.js
@@ -4,13 +4,13 @@ const Status = document.querySelector(".status");
 const Countdown = document.querySelector(".countdown");
 
 const int = setInterval(() => {
-  fetch("http://206.189.47.75:8000/status-json.xsl")
+  fetch("https://live.tomcatsquad.web.id:8443/status-json.xsl")
     .then((res) => res.json())
     .then((res) => {
     //   console.log(res["icestats"]["source"]);
       if (res["icestats"]["source"] == undefined || audio == undefined) {
         Status.innerHTML =
-        `<p>status : <span style="color : red ;">offline</span></p>` ;
+        `<p>status : <span style="color : red ;">Offline</span></p>` ;
         clearInterval(int, 100);
         tes();
         setTimeout(() => {
@@ -27,14 +27,14 @@ function tes() {
   var count = 20,
     timer = setInterval(function () {
         Countdown.style.display = "block"
-      Countdown.innerHTML = ` <p>karena radio offline web akan refresh dalam : ${count--}</p>`;
+      Countdown.innerHTML = ` <p>Karena radio offline web akan refresh dalam : ${count--}</p>`;
       Playbtn.innerHTML = `<img src="img/radio play.png">`;
       audio.pause()
       if (count == 0) clearInterval(timer);
     }, 1000);
 }
 
-let musik = "http://206.189.47.75:8000/radio";
+let musik = "https://live.tomcatsquad.web.id:8443/stream";
 let audio = new Audio();
 audio.src = musik;
 audio.play()

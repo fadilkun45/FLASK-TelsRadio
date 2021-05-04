cf03f40c39b9896de4c5b34146c1c7d353f984d5
mchevro <mchevro19@gmail.com>
2021-05-04 13:36:11 +0700

    TELS RADIO V.3.0

--- Apps/BACK-END/app.py
+++ Apps/BACK-END/app.py
@@ -1,5 +1,4 @@
 from flask import (Flask, render_template)
-import requests 
 
 app = Flask(__name__)
 
@@ -9,16 +8,7 @@ def index():
 
 @app.route('/berita')
 def index_berita():                  
-    url_youtube = requests.get('http://api-tomcatsquad.herokuapp.com/api/v1/youtube/?limit=3')
-    api_youtube = url_youtube.json()['results']
-
-    url_berita = requests.get('http://api-tomcatsquad.herokuapp.com/api/v1/news/?limit=13')
-    api_berita = url_berita.json()['results']
-    return render_template(
-        'berita.html',
-        api_youtube=api_youtube,
-        berita=api_berita,
-    )
+    return render_template('berita.html')
 
 @app.route('/musik')
 def index_musik():
